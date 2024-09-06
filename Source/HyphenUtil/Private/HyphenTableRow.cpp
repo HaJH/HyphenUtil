4981c60632c7cd0b70b602a10b08144118547981
Jeonghyeon Ha <jeonghyeon@hyphengames.com>
2024-09-06 15:54:04 +0900

    Added custom table row base

---
+++ Source/HyphenUtil/Private/HyphenTableRow.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright Hyphen Games, Inc. All Rights Reserved.
+
+
+#include "HyphenTableRow.h"
+
+void FHyphenTableRow::OnDataTableChanged(const UDataTable* InDataTable, const FName InRowName)
+{
+	RowName = InRowName;
+}
---
+++ Source/HyphenUtil/Public/HyphenTableRow.h
@@ -0,0 +1,17 @@
+﻿// Copyright Hyphen Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "HyphenTableRow.generated.h"
+
+USTRUCT(BlueprintType)
+struct HYPHENUTIL_API FHyphenTableRow : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FName RowName = NAME_None;
+	virtual void OnDataTableChanged(const UDataTable* InDataTable, const FName InRowName) override;
+};
